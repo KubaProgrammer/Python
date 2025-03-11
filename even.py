a6ea251a8152ce4c987373eaa153bfd849e01a2e
KubaProgrammer <zbiercio@gmail.com>
2025-03-11 01:14:27 +0100

    added even function

---
+++ even.py
@@ -0,0 +1,5 @@
+x = int(input("Give me a number and I'll tell if it's an even number or not.\n"))
+if x % 2 == 0:
+    print(f"{x} is an even number.")
+else:
+    print(f"{x} is not an odd number.")
